3c9987148955e24b814d5ada8ca3ea8100f1f909
Naim Berk TUMER <naimberktumer@gmail.com>
2019-03-01 19:26:48 +0300

    Finished application

--- app/src/main/java/com/hackathon/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hackathon/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.hackathon.ui.home
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -17,6 +19,15 @@ import com.hackathon.di.ILogger
 import com.hackathon.lib.builders.DialogBuilder
 import com.hackathon.ui.base.BaseFragment
 import org.koin.android.ext.android.inject
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import android.graphics.Bitmap
+import androidx.core.graphics.drawable.DrawableCompat
+import android.os.Build
+import androidx.core.content.ContextCompat
+import android.graphics.drawable.Drawable
+import android.graphics.Canvas
+import com.google.maps.android.ui.IconGenerator
 
 
 class HomeFragment : BaseFragment<HomeViewModel>(HomeViewModel::class), OnMapReadyCallback {
@@ -43,26 +54,54 @@ class HomeFragment : BaseFragment<HomeViewModel>(HomeViewModel::class), OnMapRea
         return dataBinding.root
     }
 
+    private fun getStoreVector(generator: IconGenerator, color: String): BitmapDescriptor {
+        generator.setStyle(when(color) {
+            "Yeşil" -> (IconGenerator.STYLE_GREEN)
+            "Kırmızı" -> (IconGenerator.STYLE_RED)
+            "Mavi" -> (IconGenerator.STYLE_BLUE)
+            else -> (IconGenerator.STYLE_PURPLE)
+        })
+        val icon = generator.makeIcon("$color Mağaza")
+        return BitmapDescriptorFactory.fromBitmap(icon)
+    }
 
+
+    private fun convertColor(color: String): Float {
+        return when(color) {
+            "Yeşil" -> BitmapDescriptorFactory.HUE_GREEN
+            "Kırmızı" -> BitmapDescriptorFactory.HUE_RED
+            "Mavi" -> BitmapDescriptorFactory.HUE_BLUE
+            else -> BitmapDescriptorFactory.HUE_CYAN
+        }
+    }
 
     override fun onMapReady(googleMap: GoogleMap?) {
-        val sydney1 = LatLng(-33.852, 151.211)
-        val sydney2 = LatLng(-33.852, 151.214)
-        val sydney3 = LatLng(-33.852, 151.217)
-        googleMap?.addMarker(MarkerOptions().position(sydney1)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
-                .title("Marker in Sydney"))
-        googleMap?.addMarker(MarkerOptions().position(sydney2)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-                .title("Marker in Sydney"))
-        googleMap?.addMarker(MarkerOptions().position(sydney3)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
-                .title("Marker in Sydney"))
-        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney1))
-
+        val iconFactory = IconGenerator(requireContext())
         viewModel.onProblemSolved.runWhenFinished(this,
                 onSuccess = {
                     dataBinding.progressLayout.visibility = View.GONE
+
+                    it.stores.forEach {
+                        val store = it.value.store
+
+                        googleMap?.addMarker(
+                                MarkerOptions()
+                                        .position(LatLng(store.lat, store.long))
+                                        .icon(getStoreVector(iconFactory, store.name))
+                                        .title(store.name))
+
+                        it.value.jobs.forEach {
+                            googleMap?.addMarker(
+                                    MarkerOptions()
+                                            .position(LatLng(it.lat, it.long))
+                                            .icon(BitmapDescriptorFactory.defaultMarker(convertColor(store.name)))
+                                            .title(it.id.toString())
+                                            )
+                        }
+                    }
+                    val firstStore = it.stores.values.first().store
+                    googleMap?.moveCamera(CameraUpdateFactory.zoomTo(12F))
+                    googleMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(firstStore.lat, firstStore.long)))
                 },
                 onError = { err ->
                     DialogBuilder(requireContext()).let {
